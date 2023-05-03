516f07840bfd6213133a1b73ea9a8267a6b581d1
Fabricio Rodrigues Gomes <66269944+fabriciorodrigues95@users.noreply.github.com>
2023-05-03 04:52:09 -0300

    realizado o desafio

---
+++ JS/Aual1-variaveis_operadores/aula1_operadores-variaveis.js
@@ -0,0 +1,22 @@
+// Declaração de variaveis
+let variavel1 = 10; /* let posso alterar o valor */
+// Imprimindo algo
+console.log(variavel1);
+
+variavel1 = 20;
+
+// Imprimindo algo
+console.log(variavel1);
+
+const pi=3.13; /* O const não posso alterar o seu valor */
+
+console.log(pi);
+
+let soma = 10 + 10;
+let diminuicao = 10-5;
+let mult = 10*10;
+let div = 10/10;
+
+console.log(soma, diminuicao, mult, div);
+
+
---
+++ JS/Aual1-variaveis_operadores/desafio_variacao_tempo.js
@@ -0,0 +1,9 @@
+let preco_combustivel = 5.8;
+let km_l = 12;
+let distancia_km = 20;
+
+distancia_km = distancia_km / km_l;
+
+preco_combustivel = distancia_km * preco_combustivel;
+
+console.log("O valor a ser gasto é:", preco_combustivel);
