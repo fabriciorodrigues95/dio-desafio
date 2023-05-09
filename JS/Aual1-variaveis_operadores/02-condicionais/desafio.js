9eb04533d83d212b2f3175e95fd6d0403be8c9c1
Fabricio Rodrigues Gomes <66269944+fabriciorodrigues95@users.noreply.github.com>
2023-05-08 21:58:52 -0300

    desafios
    Desafios

--- JS/Aual1-variaveis_operadores/02-condicionais/desafio.js
+++ JS/Aual1-variaveis_operadores/02-condicionais/desafio.js
@@ -1,9 +1,20 @@
-let precoEtanol = 5.49;
-let precoGasolina = 5.89;
-let escolha;
-let gastoMedioPorKm =12;
-let distanciaPorKm = 120;
-escolha = precoEtanol;
-if (escolha === precoEtanol){
-    
+let precoEtanol = 5.79;
+let precoGasolina = 6.66;
+let escolha = 1;
+let gastoMedioPorKm = 10;
+let distanciaPorKm = 100;
+
+console.log(escolha)
+if (escolha === 1) {
+    gastoMedioPorKm = (distanciaPorKm / gastoMedioPorKm)
+    escolha = (gastoMedioPorKm * precoGasolina)
+    console.log("Total da viagem é:", escolha.toFixed(2));
+} else if (escolha === 2) {
+    gastoMedioPorKm = (distanciaPorKm / gastoMedioPorKm)
+    escolha = (gastoMedioPorKm * precoEtanol)
+    console.log("Total da viagem é:", escolha.toFixed(2));
 }
+// }else {
+//     console.log("Opção Inválida");
+// }
+
